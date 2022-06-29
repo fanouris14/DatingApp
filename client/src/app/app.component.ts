fe056e988487d375b50a4ec8d73750ba72e55076
Fanouris Tsokas <fanouris.biz@gmail.com>
2022-06-29 11:48:42 +0300

    back end

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Our Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get<any>('https://localhost:5001/api/users').subscribe(
+      (res) => {
+        this.users = res;
+        console.log(this.users)
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  //?END OF CLASS
 }
